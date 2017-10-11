6a2074709e1b3ab23623ecbc055ce4f19b768205
Alessandro Sanchez <amsan7@gmail.com>
2017-10-11 12:32:45 -0700

    store ffmpeg output

--- test_scripts/get_raw_ffmpeg_output.py
+++ test_scripts/get_raw_ffmpeg_output.py
@@ -1,9 +1,13 @@
 import os
 
-DIR = "/"
+DIR = "/volume1/Experiments/XS-FACE/final-set-materials/final-videos/headcam-final/"
 
 if __name__ == "__main__":
-    pass
 
-# recursively traverse directory
-# call ffmpeg -i and save output to plain text raw_ffmpeg_output.txt (create folder)
+    for root, subdirs, filenames in os.walk(DIR):
+        for filename in filenames:
+            if filename.endswith("objs.mov"):
+
+                full_filename = os.path.join(DIR, root, filename)
+                os.system("ffmpeg -i {0} &> ~/xs-face/data/raw_ffmpeg_output/{1}.output".format(full_filename, filename))
+
