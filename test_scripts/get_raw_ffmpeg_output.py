d0176245ba69a4f93908dba02c5c932ad536431e
Alessandro Sanchez <amsan7@gmail.com>
2017-10-11 14:24:16 -0700

    extraction

--- test_scripts/get_raw_ffmpeg_output.py
+++ test_scripts/get_raw_ffmpeg_output.py
@@ -1,13 +1,14 @@
 import os
 
-DIR = "/volume1/Experiments/XS-FACE/final-set-materials/final-videos/headcam-final/"
+DATA_DIR = "/volume1/Experiments/XS-FACE/final-set-materials/final-videos/headcam-final"
+OUTPUT_DIR = "/var/services/homes/alessandro/xs-face/data/raw_ffmpeg_output"
 
 if __name__ == "__main__":
 
-    for root, subdirs, filenames in os.walk(DIR):
+    for root, subdirs, filenames in os.walk(DATA_DIR):
         for filename in filenames:
             if filename.endswith("objs.mov"):
-
-                full_filename = os.path.join(DIR, root, filename)
-                os.system("ffmpeg -i {0} &> ~/xs-face/data/raw_ffmpeg_output/{1}.output".format(full_filename, filename))
+                print("running ffmpeg on {}...".format(filename))
+                full_filename = os.path.join(DATA_DIR, root, filename)
+                os.system("ffmpeg -i {} &> {}/{}.output".format(full_filename, OUTPUT_DIR, filename))
 
