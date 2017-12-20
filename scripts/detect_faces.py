fecd3ad2edfba5a9c0f4587a9707197e3a8bbd66
Alessandro Miguel Sanchez <sanchez7@sherlock-ln04.stanford.edu>
2017-12-20 14:01:16 -0800

    change slurm parameters

--- scripts/detect_faces.py
+++ scripts/detect_faces.py
@@ -19,7 +19,7 @@ def detect_faces(gray_img):
     return faceCascade.detectMultiScale(
         gray_img,
         scaleFactor=1.1,
-        minNeighbors=5,
+        minNeighbors=20,
         minSize=(30, 30)
     )
 
@@ -32,8 +32,7 @@ def rotate_img(image, angle):
     return result
 
 
-def process_img(imgpath, i, arg):
-    # print('batch {0}: {1}%'.format(arg, i/1075.0 * 100))
+def process_img(imgpath):
     print imgpath
     gray_img = cv2.cvtColor(cv2.imread(imgpath), cv2.COLOR_BGR2GRAY)
 
@@ -47,38 +46,38 @@ def process_img(imgpath, i, arg):
     rows = []
 
     if len(faces) == 0:
-        rows.append([group, name, frame, False, None, None, None, None, 0])
-    else:
-        for (x, y, w, h) in faces:
-            rows.append([group, name, frame, True, x, y, w, h, 0])
-
-    if len(faces_rotated) == 0:
         rows.append([group, name, frame, False, None, None, None, None, 180])
     else:
-        for (x, y, w, h) in faces_rotated:
+        for (x, y, w, h) in faces:
             rows.append([group, name, frame, True, x, y, w, h, 180])
+
+    if len(faces_rotated) == 0:
+        rows.append([group, name, frame, False, None, None, None, None, 0])
+    else:
+        for (x, y, w, h) in faces_rotated:
+            rows.append([group, name, frame, True, x, y, w, h, 0])
 
     return rows
 
 
 if __name__ == "__main__":
-    img_index = int(sys.argv[1]) * 1075
+    img_index = int(sys.argv[1]) * 4171
 
     with open(IMAGE_PATHS_FILE, 'rb') as img_paths_f:
         img_paths = list(csv.reader(img_paths_f, delimiter=','))
-        images_to_process = img_paths[img_index-1074:img_index+1]
+        images_to_process = img_paths[img_index-4170:img_index+1]
 
     pool = multiprocessing.Pool()
     results = []
 
-    for i, row in enumerate(images_to_process):
-        results.append(pool.apply_async(process_img, args=(row[0], i, sys.argv[1])))
+    for row in images_to_process:
+        results.append(pool.apply_async(process_img, args=(row[0],)))
 
     pool.close()
 
     with open(OUTPUT_FILE % sys.argv[1], 'wb') as csvfile:
         wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        wr.writerow(['group', 'video', 'frame', 'is_face', 'x', 'y', 'w', 'h', 'angle'])
+        # wr.writerow(['group', 'video', 'frame', 'is_face', 'x', 'y', 'w', 'h', 'angle'])
 
         for result in results:
             try:
--- scripts/wrap.sh
+++ scripts/wrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in $(seq 679 $END); 
-do sbatch -p normal,hns -t 40:00 -c 16 --mail-type=FAIL --mail-user=sanchez7@stanford.edu --wrap="python detect_faces.py $i"
+for i in $(seq 175 $END);
+do sbatch -p normal,hns -t 40:00 -c 8 --mail-type=FAIL --mail-user=sanchez7@stanford.edu --wrap="python detect_faces.py $i"
 sleep 1 
 done
